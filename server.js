a317767cb027ad88e2d329e98c0abe51e43502de
Janadari <31371187+janadari@users.noreply.github.com>
2021-02-24 19:04:02 +0530

    updated

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-   res.json({"message": "Welcome to ExpressMongoApp application. Created by IT Jugadu"});
+   res.json({"message": "Welcome to ExpressMongoApp application. "});
 });
 
 // Require charts routes
